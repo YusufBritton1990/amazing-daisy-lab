a88b590b8e0a7a4f36fc2dc0c53b8d0f14034b61
Yusuf <yusufbritton@gmail.com>
2023-10-04 18:54:58 -0400

    initial commit

---
+++ exercise.py
@@ -0,0 +1,55 @@
+# print('hello')
+# print('world')
+
+word_one = "abcdegf"
+# print(word)
+
+# print(len(word))
+
+# Start
+# result = word[0]
+# print(result)
+
+# Start from another index
+# result = word[2]
+# print(result)
+
+# Range from start and stop
+# result = word[0:2]
+# print(result)
+
+# Start, Stop, and Step
+# result = word[2:8:2]
+# print(result)
+
+# result = word[-2]
+# print(result)
+
+# Splicing
+# word = 'Greetings'
+# print(word)
+# print(word[0])
+# print(word[0:3])
+# print(word[2:4])
+# print(len(word))
+# print(word[0:len(word)])
+# print(word[-1])
+# print(word[-4:-2])
+#
+# # Concatenation
+# print(word + word_one)
+#
+# # Arithmetic
+# print(1 + 2)
+# print(1.5 + 2.3)
+# print(word * 3)
+#
+# print(word.find('t'))
+# print(word.find('q'))
+
+# example
+word = 'pie'
+word_two = 'tree'
+
+print(word.find('e'))
+print(word_two.find('e'))
